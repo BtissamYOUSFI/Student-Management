168b892d3e98c576de8daeb2a0f035b06132555e
YOUSFI Btissam <ibtissamyousfi111@gmail.com>
2026-03-07 15:39:06 +0000

    Edit DBConnection Class

--- src/main/java/ma/ensa/studentmanagement/db/DBConnection.java
+++ src/main/java/ma/ensa/studentmanagement/db/DBConnection.java
@@ -11,12 +11,20 @@ public final class DBConnection {
 
     public static Connection getInstance() {
         if (conn == null) {
-            try{
+            try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
+
+                String host = System.getenv("MYSQL_HOST");
+                String port = System.getenv("MYSQL_PORT");
+                String db   = System.getenv("MYSQL_DATABASE");
+                String user = System.getenv("MYSQL_USER");
+                String pass = System.getenv("MYSQL_PASSWORD");
+
                 conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/studentmanagement?useSSL=false",
-                        "root",
-                        "");
+                        "jdbc:mysql://" + host + ":" + port + "/" + db + "?useSSL=false",
+                        user,
+                        pass
+                );
             } catch (ClassNotFoundException | SQLException e) {
                 throw new RuntimeException(e);
             }
